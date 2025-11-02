769ac378a4862953c8d6f5a0aa01c3c57d359112
fyneboy001 <fyneboyfynerose@gmail.com>
2025-11-02 17:57:30 +0100

    My first commit

---
+++ Backend/prisma/connection.ts
@@ -0,0 +1,15 @@
+// prisma/connection.prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
+
+// Prevent multiple instances of Prisma Client in development
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
